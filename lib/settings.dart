009581df566a3897afd90493248e9ea6dd08d084
rahulg221 <125697931+rahulg221@users.noreply.github.com>
2023-08-05 12:25:01 -0400

    Add files via upload

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'main.dart';
@@ -408,6 +409,10 @@ class _MySettingsPageState extends ConsumerState<MySettingsPage> {
                 : Colors.white,
           ),
         ),
+        keyboardType: TextInputType.numberWithOptions(decimal: true),
+        inputFormatters: <TextInputFormatter>[
+          FilteringTextInputFormatter.allow(RegExp(r'^\d+\.?\d*')),
+        ],
       );
 
   Widget _tSlider(double currentTol) => SliderTheme(
